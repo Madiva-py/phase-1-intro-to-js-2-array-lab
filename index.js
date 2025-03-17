a667e41a7f08cdd8728c13ba126126906dc005d4
michael <aoinihonjin@gmail.com>
2025-03-17 20:08:43 +0300

    idk what i did to the other files god help us all

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+let cats=  ["Milo", "Otis", "Garfield"]
+console.log(cats["1"])//this is the array
+
+cats.push("vamose")
+cats.unshift("evelyn")
+cats.pop("vamose")
+cats.shift("mylo")

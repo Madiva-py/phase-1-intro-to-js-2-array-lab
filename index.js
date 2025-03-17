3ad5a12d5151a0d0104eee37b8cc4cb23d760669
michael <aoinihonjin@gmail.com>
2025-03-17 20:57:20 +0300

    added this crazy js modification

--- index.js
+++ index.js
@@ -2,7 +2,32 @@
 let cats=  ["Milo", "Otis", "Garfield"]
 console.log(cats["1"])//this is the array
 
-cats.push("vamose")
-cats.unshift("evelyn")
-cats.pop("vamose")
-cats.shift("mylo")
+function destructivelyAppendCat(ralph) {
+    cats.push(ralph); 
+}
+function  destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+    }
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();  
+}
+function appendCat(Broom) {
+    return [...cats, Broom];  
+}
+
+function prependCat(vamoose) {
+    return [vamoose, ...cats]; 
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);  
+}
+
+function removeFirstCat() {
+    return cats.slice(1);  
+}
+
+
